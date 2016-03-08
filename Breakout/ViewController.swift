4929e8e79ffa623f55ee5d81d021c75ff9b2175f
Michael Maczynski <mikemaczynski@michaels-macbook-pro.local>
2016-03-08 06:48:49 -0600

    Added paddle

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -12,15 +12,12 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     
     var dynamicAnimator = UIDynamicAnimator()
-    var dynamicAnimator2 = UIDynamicAnimator()
-    var snapBehaviour:UISnapBehavior!
+    var collisionBehavior = UICollisionBehavior()
+    var ball:UIView!
+    var paddle:UIView!
+    
+    let panRec = UIPanGestureRecognizer()
 
-    
-    //distance between blocks horizontally
-    var dbbh:CGFloat = 10.0
-    
-    //distance between blocks vertically
-    let dbbv:CGFloat = 5.0
     
     //length of block
     let lob:CGFloat = 50.0
@@ -29,194 +26,122 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     let hob:CGFloat = 15.0
     
     //columns
-    let columns = 12
+    let columns = 13
     
     //rows
-    let rows: CGFloat = 1
-    
-    //initial distance
-    let id: CGFloat = 30.0
-    
-    
-    //mutiplier row 1
-    var m1h: CGFloat = 1.0
-    
-    //mutiplier row 2
-    var m2: CGFloat = 1.0
-
-    //mutiplier row 2 vertical
-    var m2v: CGFloat = 1.0
-    
-    //mutiplier row 3
-    var m3: CGFloat = 1.0
-    
-    //mutiplier row 3 vertical
-    var m3v: CGFloat = 1.0
-    
-    //mutiplier row 4
-    var m4: CGFloat = 1.0
-    
-    //mutiplier row 4 vertical
-    var m4v: CGFloat = 1.0
-    
-  //  var bricks : [UIView] = []
+    let rows: CGFloat = 4
 
     var blocks:[UIView] = []
     var startBallArray:[UIView] = []
-    var test:[UIView] = []
-
+    var allViews:[UIView] = []
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         dynamicAnimator = UIDynamicAnimator(referenceView: view)
         
+        var x:CGFloat = 0
+        var y:CGFloat = 30
+        
+        for _ in 1...4
+        {
+            for _ in 1...13
+            {
+                let block = UIView(frame: CGRectMake(x, y, lob, hob))
+                block.backgroundColor = UIColor.blackColor()
+                view.addSubview(block)
+                
+                blocks.append(block)
+                allViews.append(block)
+                
+                x += 60
+            }
+            x = 0
+            y += 30
+        }
+
         
         
-        setupViews()
+        let ball = UIView(frame: CGRectMake(300, 300, 20, 20))
+        ball.backgroundColor = UIColor.redColor()
+        view.addSubview(ball)
+        ball.layer.cornerRadius = ball.frame.size.width/2
+        ball.clipsToBounds = true
+        startBallArray.append(ball)
+        allViews.append(ball)
+
+        
+        let paddle = UIView(frame: CGRectMake(260, 600, 70, 10))
+        paddle.backgroundColor = UIColor.blackColor()
+        view.addSubview(paddle)
+        allViews.append(paddle)
+
+        
+        panRec.addTarget(self, action: "draggedView:")
+        paddle.addGestureRecognizer(panRec)
+
+        
+        
+        setupBehaviors()
         
     }
-    
-    
-    
-    
-    
-    
    
-        func setupViews()
+        func setupBehaviors()
         {
+            //let boundary = UIView(frame: CGRectMake(0, 600, 1000, 15))
+            //boundary.backgroundColor = UIColor.blackColor()
+            //view.addSubview(boundary)
+            //blocks.append(boundary)
             
-            //row1
-            let sq1 = UIView(frame: CGRectMake(0, id, lob, hob))
-            sq1.backgroundColor = UIColor.blackColor()
-            view.addSubview(sq1)
-            blocks.append(sq1)
             
-            for _ in 0...columns
-            {
-                let sq12 = UIView(frame: CGRectMake(m1h * (lob + dbbh), id, lob, hob))
-                sq12.backgroundColor = UIColor.blackColor()
-                view.addSubview(sq12)
-                m1h++
-                blocks.append(sq12)
-                
-            }
-            
-            //row2
-            
-            let sq2 = UIView(frame: CGRectMake(0, 55, lob, hob))
-            sq2.backgroundColor = UIColor.blackColor()
-            view.addSubview(sq2)
-            blocks.append(sq2)
-            
-            for _ in 0...columns
-            {
-                let sq22 = UIView(frame: CGRectMake(m2 * (lob + dbbh), 55, lob, hob))
-                sq22.backgroundColor = UIColor.blackColor()
-                view.addSubview(sq22)
-                m2++
-                m2v++
-                blocks.append(sq22)
-                
-            }
-            
-            //row3
-            
-            let sq3 = UIView(frame: CGRectMake(0, 25 + 55, lob, hob))
-            sq3.backgroundColor = UIColor.blackColor()
-            view.addSubview(sq3)
-            blocks.append(sq3)
-            
-            for _ in 0...columns
-            {
-                let sq32 = UIView(frame: CGRectMake(m3 * (lob + dbbh), 25 + 55, lob, hob))
-                sq32.backgroundColor = UIColor.blackColor()
-                view.addSubview(sq32)
-                m3++
-                m3v++
-                blocks.append(sq32)
-                
-            }
-            
-            //row4
-            
-            let sq4 = UIView(frame: CGRectMake(0, 50 + 55, lob, hob))
-            sq4.backgroundColor = UIColor.blackColor()
-            view.addSubview(sq4)
-            blocks.append(sq4)
-            
-            for _ in 0...columns
-            {
-                let sq42 = UIView(frame: CGRectMake(m4 * (lob + dbbh), 50 + 55, lob, hob))
-                sq42.backgroundColor = UIColor.blackColor()
-                view.addSubview(sq42)
-                m4++
-                m4v++
-                view.addSubview(sq4)
-                blocks.append(sq42)
-                
-                
-            }
-        
-            
-            let boundary = UIView(frame: CGRectMake(0, 600, 1000, 15))
-            boundary.backgroundColor = UIColor.blackColor()
-            view.addSubview(boundary)
-            blocks.append(boundary)
-            
-        
-            
-            let startBall = UIView(frame: CGRectMake(300, 300, 20, 20))
-            startBall.backgroundColor = UIColor.redColor()
-            view.addSubview(startBall)
-            startBall.layer.cornerRadius = startBall.frame.size.width/2
-            startBall.clipsToBounds = true
-            startBallArray.append(startBall)
-            
-            /////////
-            
-            var bothArray = blocks + startBallArray
-            var pushBoth = blocks + test + bothArray
-        
         let dynamicItemBehavior = UIDynamicItemBehavior(items: blocks)
         dynamicItemBehavior.density = 1000000.0
-        dynamicItemBehavior.friction = 100000000.0
-        dynamicItemBehavior.resistance = 10000000000.0
-        //dynamicItemBehavior.elasticity = 1.3
+        dynamicItemBehavior.elasticity = 1.0
         dynamicItemBehavior.allowsRotation = false
         dynamicAnimator.addBehavior(dynamicItemBehavior)
         
         let ballDynamicItemBehavior = UIDynamicItemBehavior(items: startBallArray)
-        dynamicItemBehavior.density = 1.0
         ballDynamicItemBehavior.friction = 0.0
         ballDynamicItemBehavior.resistance = 0.0
-        ballDynamicItemBehavior.elasticity = 1.3
+        ballDynamicItemBehavior.elasticity = 1.2
         dynamicAnimator.addBehavior(ballDynamicItemBehavior)
 
-            
-        let collisionBehavior = UICollisionBehavior(items: bothArray)
+        let collisionBehavior = UICollisionBehavior(items: allViews)
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
         collisionBehavior.collisionMode = .Everything
         collisionBehavior.collisionDelegate = self
         dynamicAnimator.addBehavior(collisionBehavior)
-        
-        
-        
+            
         let pushBehavior = UIPushBehavior(items: startBallArray, mode: .Continuous)
         pushBehavior.magnitude = 0.3
         pushBehavior.pushDirection = CGVectorMake(0, 0.9)
         dynamicAnimator.addBehavior(pushBehavior)
-        
+
         
     }
     
+    @IBAction func paddleMovement(sender: UIPanGestureRecognizer) {
+              // dynamicAnimator.updateItemUsingCurrentState(paddle)
+    }
     
-    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item1: UIDynamicItem, withItem item2: UIDynamicItem, atPoint p: CGPoint) {
-        
-        
-        print(p)
-        
-        
+    func draggedView(sender:UIPanGestureRecognizer){
+        self.view.bringSubviewToFront(sender.view!)
+        var translation = sender.translationInView(self.view)
+        sender.view!.center = CGPointMake(sender.view!.center.x + translation.x, sender.view!.center.y + translation.y)
+        sender.setTranslation(CGPointZero, inView: self.view)
+
+    }
+    
+    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item1: UIDynamicItem, withItem item2: UIDynamicItem, atPoint p: CGPoint)
+    {
+        //print(blocks)
+
+        for block in blocks
+        {
+           // print(block)
+        }
+
         
     }
 
@@ -255,107 +180,3 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-for (var brickNum = 0; brickNum < BRICK_COLUMNS * BRICK_ROWS; brickNum++) {
-
-let column = brickNum % BRICK_COLUMNS
-let row = brickNum / BRICK_ROWS
-
-let x = width * CGFloat(column) + CGFloat(BRICK_MARGIN) * CGFloat(column)
-let y = height * CGFloat(row) + CGFloat(BRICK_MARGIN) * CGFloat(row)
-
-let brick = UIView(frame: CGRectMake(x, y, width, height))
-
-if(row <= 1) {
-brick.backgroundColor = UIColor.greenColor()
-} else if(row <= 3) {
-brick.backgroundColor = UIColor.yellowColor()
-} else {
-brick.backgroundColor = UIColor.redColor()
-}
-
-view.addSubview(brick)
-
-bricks.append(brick)
-
-let brickDynamicBehavior = UIDynamicItemBehavior(items: [brick])
-brickDynamicBehavior.density = 10000
-brickDynamicBehavior.resistance = 100
-brickDynamicBehavior.allowsRotation = false
-dynamicAnimator.addBehavior(brickDynamicBehavior)
-
-}
-
-
-
-
-
-/*
-
-let sq1 = UIView(frame: CGRectMake(0, 50, lob, hob))
-sq1.backgroundColor = UIColor.blackColor()
-view.addSubview(sq1)
-let sq2 = UIView(frame: CGRectMake(lob + dbbh, 50, lob, hob))
-sq2.backgroundColor = UIColor.redColor()
-view.addSubview(sq2)
-
-let sq3 = UIView(frame: CGRectMake(2 * (lob + dbbh), 50, lob, hob))
-sq3.backgroundColor = UIColor.blackColor()
-view.addSubview(sq3)
-
-let sq4 = UIView(frame: CGRectMake(3 * (lob + dbbh), 50, lob, hob))
-sq4.backgroundColor = UIColor.blackColor()
-view.addSubview(sq4)
-
-let sq5 = UIView(frame: CGRectMake(4 * (lob + dbbh), 50, lob, hob))
-sq5.backgroundColor = UIColor.redColor()
-view.addSubview(sq5)
-
-let sq6 = UIView(frame: CGRectMake(5 * (lob + dbbh), 50, lob, hob))
-sq6.backgroundColor = UIColor.redColor()
-view.addSubview(sq6)
-
-let sq7 = UIView(frame: CGRectMake(6 * (lob + dbbh), 50, lob, hob))
-sq7.backgroundColor = UIColor.blackColor()
-view.addSubview(sq7)
-
-let sq8 = UIView(frame: CGRectMake(7 * (lob + dbbh), 50, lob, hob))
-sq8.backgroundColor = UIColor.blackColor()
-view.addSubview(sq8)
-
-let sq9 = UIView(frame: CGRectMake(8 * (lob + dbbh), 50, lob, hob))
-sq9.backgroundColor = UIColor.redColor()
-view.addSubview(sq9)
-
-let sq10 = UIView(frame: CGRectMake(9 * (lob + dbbh), 50, lob, hob))
-sq10.backgroundColor = UIColor.redColor()
-view.addSubview(sq10)
-
-let sq11 = UIView(frame: CGRectMake(10 * (lob + dbbh), 50, lob, hob))
-sq11.backgroundColor = UIColor.redColor()
-view.addSubview(sq11)
-
-let sq12 = UIView(frame: CGRectMake(11 * (lob + dbbh), 50, lob, hob))
-sq12.backgroundColor = UIColor.blackColor()
-view.addSubview(sq12)
-
-let sq13 = UIView(frame: CGRectMake((12 * (lob + dbbh)) - 2, 50, lob, hob))
-sq13.backgroundColor = UIColor.blackColor()
-view.addSubview(sq13)
-*/
-*/
